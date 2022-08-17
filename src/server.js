e6d669bd1899cb2f97309193b7d97d02699669c7
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-08-17 08:00:36 +0100

    Added

--- src/server.js
+++ src/server.js
@@ -1,35 +1,38 @@
-const dotenv = require('dotenv').config()
-const mongoose = require('mongoose')
-const express = require('express')
-const port = process.env.PORT
-const dbURI = process.env.MONGO_URI
-const app = express()
-
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const port = process.env.PORT;
+const dbURI = process.env.MONGO_URI;
+const app = express();
 
 //MongoDB connection
-const conn = mongoose.connect(dbURI, {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(()=>{
-    console.log('MongoDB Connected')
-})
-.catch((err)=>{
-    console.log(err)
-})
-
-//Middleware setup
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
-// //routes
-// app.use('/api/auth', require('./routes/authRoutes/authUserRoutes'))
-// app.use('/api/user', require('./routes/userRoutes/userRoute'))
-// app.use('/api/property', require('./routes/propertyRoutes/propertyRoute'))
-// app.use('/api/blog', require('./routes/blogRoutes/blogRoute'))
-
-app.use("*", (req, res) => {
-    return res.status(404).json({ error: { messgage: "Route Not Found" } });
+const conn = mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-app.listen(port, console.log(`server running on ${port}`))
+//Middleware setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  return res.status(200).send("We're up and running!!");
+});
+app.use("/api/auth", require("./routes/authRoutes/authUserRoutes"));
+app.use("/api/user", require("./routes/userRoutes/userRoute"));
+app.use("/api/property", require("./routes/propertyRoutes/propertyRoute"));
+app.use("/api/blog", require("./routes/blogRoutes/blogRoute"));
+
+app.use("*", (req, res) => {
+  return res.status(404).json({ error: { messgage: "Route Not Found" } });
+});
+
+app.listen(port, console.log(`server running on ${port}`));
