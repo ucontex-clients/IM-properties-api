18402f270af3271eeea6b6373e1619d11c5b2a86
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-08-17 08:05:58 +0100

    Added Some

--- src/controllers/authController/authUserController.js
+++ src/controllers/authController/authUserController.js
@@ -1,55 +1,62 @@
-const User = require('../../models/UserSchema')
-const userSchemaValidation = require('../../utils/validateUserSchema')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const validateUserLogin = require('../../utils/validateUserLogin')
-const tokenSecret = process.env.TOKEN_SECRET
+const User = require("../../models/UserSchema");
+const userSchemaValidation = require("../../utils/validateUserSchema");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateUserLogin = require("../../utils/validateUserLogin");
+const tokenSecret = process.env.TOKEN_SECRET;
 
-const registerController = async(req, res) => {
-try {
+const registerController = async (req, res) => {
+  try {
+    let { body } = req;
 
-    let {body} = req
-    console.log(body)
-    const {error, value} = userSchemaValidation(body)
-    if(error){
-         return res.status(400).json({error:{message: error.details[0].message}})
+    const { error, value } = userSchemaValidation(body);
+    if (error) {
+      return res
+        .status(400)
+        .json({ error: { message: error.details[0].message } });
     }
-    const userExist = await User.findOne({email:body.email})
-    if(userExist){
-        return res.status(400).json({error:{message:'Hey!! we already have you on board. Simply login'}})
+    const userExist = await User.findOne({ email: body.email });
+    if (userExist) {
+      return res
+        .status(400)
+        .json({
+          error: { message: "Hey!! we already have you on board. Simply login" }
+        });
     }
-    const salt =await bcrypt.genSalt(10)
-    body.password = await bcrypt.hash(body.password,salt)
+    const salt = await bcrypt.genSalt(10);
+    body.password = await bcrypt.hash(body.password, salt);
 
-    const newUser =await User.create(body)
-        return  res.status(200).json(newUser)
-    
-} catch (error) {
-     res.status(500).json(error)
-}
-}
+    const newUser = await User.create(body);
+    return res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const loginController = async(req, res) => {
-    try {
-     const {body} = req
+const loginController = async (req, res) => {
+  try {
+    const { body } = req;
     //  console.log(body)
-     const {error, value} = validateUserLogin(body)
+    const { error, value } = validateUserLogin(body);
     //  console.log(value)
-     if(error){
-         res.status(400).json({error:{message: error.details[0].message}})
-     }
-     const user = await User.findOne({email:body.email}).select('+password')
-     const isPassword = await bcrypt.compare(body.password,user.password)
-     if(user && isPassword   ){
-        const token = jwt.sign({_id:user._id, role:user.role},tokenSecret, {expiresIn:'30d'})
-        return res.status(200).json({message:"Login Successful",id:user._id, token})
-     }else{
-         res.status(400).json({message:"invalid username and password"})    
-     }   
-    } catch (error) {
-     res.status(500).json({error:{message: error}})
-     console.log(error)
+    if (error) {
+      res.status(400).json({ error: { message: error.details[0].message } });
     }
- 
- }
-module.exports = {registerController,loginController}
+    const user = await User.findOne({ email: body.email }).select("+password");
+    const isPassword = await bcrypt.compare(body.password, user.password);
+    if (user && isPassword) {
+      const token = jwt.sign({ _id: user._id, role: user.role }, tokenSecret, {
+        expiresIn: "30d"
+      });
+      return res
+        .status(200)
+        .json({ message: "Login Successful", id: user._id, token });
+    } else {
+      res.status(400).json({ message: "invalid username and password" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: { message: error } });
+    console.log(error);
+  }
+};
+module.exports = { registerController, loginController };
