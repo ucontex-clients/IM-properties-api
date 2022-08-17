ef9c30788f76bc4d62279d57cf40010ea6b6cd77
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-08-17 08:27:46 +0100

    Added a fix

--- src/models/BlogSchema.js
+++ src/models/BlogSchema.js
@@ -1,34 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const BlogSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const BlogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
   },
-  addedBy:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: User
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true
-  },
-}, { timestamps: true });
-
+  { timestamps: true }
+);
 
 const populateUser = function (next) {
-    this.populate("addedBy", "_id email firstname lastname username image");
-    next();
-  };
-  
-  BlogSchema.pre("find", populateUser)
-    .pre("findOne", populateUser)
-    .pre("findOneAndUpdate", populateUser);
+  this.populate("addedBy", "_id email firstname lastname username image");
+  next();
+};
 
-const Blog = mongoose.model('Blog', BlogSchema);
+BlogSchema.pre("find", populateUser)
+  .pre("findOne", populateUser)
+  .pre("findOneAndUpdate", populateUser);
+
+const Blog = mongoose.model("Blog", BlogSchema);
 module.exports = Blog;
