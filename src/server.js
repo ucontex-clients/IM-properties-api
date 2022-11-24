476a8494351fb0351feddc926d7d9ed839b9174a
bootspace <gregudogu@gmail.com>
2022-11-24 13:39:53 +0100

    Edited CORS setup and Signup Validator

--- src/server.js
+++ src/server.js
@@ -2,17 +2,23 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const port = require("./config/constant").PORT
-const dbURI = require("./config/constant").mongoURI;
+const dbURI = require("./config/constant").mongoURI2;
 const cors = require('cors')
 const app = express();
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://improperties.herokuapp.com",
-  ],
+  origin: '*',
+  // [
+  //   "http://localhost:3000",
+  //   "https://improperties.herokuapp.com",
+  // ],
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}))
 app.use( cors(corsOptions));
 
 
