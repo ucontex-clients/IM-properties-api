9f33aa038eef8332d749c31672a17eeb14232c15
bootspace <gregudogu@gmail.com>
2022-12-07 03:59:14 +0100

    Edited the PaymentRoutes files that was giving issues

--- src/routes/paymentRoutes/paymentRoute.js
+++ src/routes/paymentRoutes/paymentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { uploadPhotos } = require('../../config/multer');
+// const { uploadPhotos } = require('../../config/multer');
 const { cloudinary } = require('../../config/cloudinaryConfig');
 const verifyToken = require('../../middleware/authMiddleware/verifyToken')
 const fs = require('fs');
@@ -13,43 +13,43 @@ router.post('/paymentflutterwave/:property/:transaction_url', verifyToken, Payme
 
 
 
-router.post('/offlinepayment/:property', uploadPhotos.single('receipt'), async (req, res) => {
-  const {_id} = req.user;
-  const property = req.params.property;
-  let file = req.file.path;
-  try {
-    if(!req.file){
-      console.log('Upload a valid Bank Receipt');
-      return res.status(404).json('Please add a valid Bank receipt');
-    }
-    const upload = await cloudinary.uploader.upload(file);
-    const { mode, duration } = req.body;
+// router.post('/offlinepayment/:property', uploadPhotos.single('receipt'), async (req, res) => {
+//   const {_id} = req.user;
+//   const property = req.params.property;
+//   let file = req.file.path;
+//   try {
+//     if(!req.file){
+//       console.log('Upload a valid Bank Receipt');
+//       return res.status(404).json('Please add a valid Bank receipt');
+//     }
+//     const upload = await cloudinary.uploader.upload(file);
+//     const { mode, duration } = req.body;
 
-    const uploadedReceipt = await BankTransfer.create({
-      Payer: _id,
-      Property: property,
-      uploadUrl: upload.public_id,
-      mode,
-      duration
-    });
+//     const uploadedReceipt = await BankTransfer.create({
+//       Payer: _id,
+//       Property: property,
+//       uploadUrl: upload.public_id,
+//       mode,
+//       duration
+//     });
 
-    if(!uploadedReceipt){
-      console.log('Failed to update Bank Receipt payment');
-      return res.status(404).json('Failed to update Bank Receipt payment');
-    }
+//     if(!uploadedReceipt){
+//       console.log('Failed to update Bank Receipt payment');
+//       return res.status(404).json('Failed to update Bank Receipt payment');
+//     }
 
-    console.log(uploadedReceipt);
-    return res.status(200).json(uploadedReceipt);
+//     console.log(uploadedReceipt);
+//     return res.status(200).json(uploadedReceipt);
 
-    fs.unlink(file, (err) => {
-      if(err) throw err;
-      console.log("File succesfully deleted")
-    })
+//     fs.unlink(file, (err) => {
+//       if(err) throw err;
+//       console.log("File succesfully deleted")
+//     })
 
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json(error.message);
-  }
-})
+//   } catch (error) {
+//     console.log(error.message);
+//     return res.status(500).json(error.message);
+//   }
+// })
 
 module.exports = router;
